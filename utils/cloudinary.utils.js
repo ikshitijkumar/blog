80d6f8d2c3735721e29834505bffea304ea5ca75
Kshitij Kumar <97625662+ikshitijkumar@users.noreply.github.com>
2024-03-22 23:36:02 +0530

    Update cloudinary.utils.js

--- utils/cloudinary.utils.js
+++ utils/cloudinary.utils.js
@@ -12,31 +12,22 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null
-        //upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        // file has been uploaded successfull
-        //console.log("file is uploaded on cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
         return response;
 
     } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        fs.unlinkSync(localFilePath) 
         return null;
     }
 }
 
 const deleteFromCloudinary = async (imageUrl) =>{
     try {
-      // Extract the public ID from the image URL
-      // const publicId = cloudinary.utils.publicId(imageUrl);
       const publicId = imageUrl.split('/').pop().split('.')[0];
-      // Delete the image by public ID
       const result = await cloudinary.uploader.destroy(publicId);
-      //   console.log(result);
-  
-      // Handle the result - check if the deletion was successful
       if (result.result === 'ok') {
         console.log('Old Image deleted successfully');
       } else {
